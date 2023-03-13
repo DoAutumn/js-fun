80db8813b8e641ab15dc2dec63a161cb899ab157
井方 <doautumn@163.com>
2023-03-13 18:52:06 +0800

    update dist

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import App from './App.vue'
 
 import 'element-plus/theme-chalk/index.css'
 
+import hljs from 'highlight.js/lib/core'
+import javascript from 'highlight.js/lib/languages/javascript'
+hljs.registerLanguage('javascript', javascript)
 import hljsVuePlugin from '@highlightjs/vue-plugin'
 import 'highlight.js/styles/stackoverflow-light.css'
 import 'highlight.js/lib/common'
